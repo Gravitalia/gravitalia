66c5102ad7f0ea8c9538840ea6922f65f156e131
RealHinome <acn@gravitalia.com>
2025-09-22 08:34:05 +0200

    Fix responsive

--- app/components/Home/Examples.vue
+++ app/components/Home/Examples.vue
@@ -1,9 +1,13 @@
 <template>
-    	<div
+	<div
 		class="h-full max-h-xl w-full py-12 xl:py-24 px-10 xl:px-56 grid grid-cols-1 xl:grid-cols-2 place-items-center gap-y-6"
 	>
-		<div class="mr-48">
-			<MockupMacbook class="h-[280px] w-[450px] lg:h-[425px] lg:w-[650px]" sclass="w-[500px] h-4 lg:w-[740px] lg:h-6" src="/mockup/turms.png" />
+		<div class="lg:mr-48">
+			<MockupMacbook
+				class="h-[280px] w-[450px] lg:h-[425px] lg:w-[650px]"
+				sclass="w-[500px] h-4 lg:w-[740px] lg:h-6"
+				src="/mockup/turms.png"
+			/>
 		</div>
 		<div class="lg:ml-24 max-w-md xl:max-w-lg">
 			<h3 class="font-semibold text-violet-500 dark:text-violet-400 uppercase">
