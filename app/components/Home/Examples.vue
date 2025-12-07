29b423ee7271e60d89215b1e8ebdb46dafee448c
RealHinome <acn@gravitalia.com>
2025-12-07 13:43:29 +0100

    Align mockups

--- app/components/Home/Examples.vue
+++ app/components/Home/Examples.vue
@@ -20,7 +20,7 @@
 	<div
 		class="h-full max-h-xl w-full py-12 xl:py-24 px-10 xl:px-56 2xl:px-80 grid grid-cols-1 xl:grid-cols-2 place-items-center gap-y-6"
 	>
-		<div class="lg:mr-48">
+		<div class="lg:mr-48 xl:order-1">
 			<MockupMacbook
 				class="h-44 w-64 lg:h-[425px] lg:w-[650px]"
 				sclass="w-80 h-3 lg:w-[740px] lg:h-6"
@@ -35,7 +35,7 @@
 					src="/mockup/turms.png"
 			/></MockupMacbook>
 		</div>
-		<div class="lg:ml-24 max-w-md xl:max-w-lg">
+		<div class="lg:ml-24 max-w-md xl:max-w-lg xl:order-2">
 			<h3 class="font-semibold text-violet-500 dark:text-violet-400 uppercase">
 				{{ $t("home.computer.title") }}
 			</h3>
